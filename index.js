d0f3a366670ba2a31ef258e7f5d6812c492ad110
benito <muciroben@gmail.com>
2020-09-27 21:45:06 +0200

    end

--- index.js
+++ index.js
@@ -1,68 +1,64 @@
-//jquery method
+// jquery method 
 $(document).ready(function(){
-    //technology owl carousel
+
+    // technology owl carousel
     $('#tech .owl-carousel').owlCarousel({
-        margin:40,
-        dots:false,
-        nav:true,
+        nav: true,
+        dots: false,
+        margin: 40,
         responsive:{
             0:{
                 items:1
             },
-
             600:{
                 items:2
             },
             1000:{
                 items:3
-            },
-
-
+            }
         }
     });
-     //security owl carousel
-     $('#security .owl-carousel').owlCarousel({
-        margin:40,
-        dots:false,
-        nav:true,
-        loop:true,
-        navText:["<span><i class='fas fa-arrow-left'></i>Previous</span>","<span>Next<i class='fas fa-arrow-right'></i></span>"],
+
+    // security owl carousel
+    $('#security .owl-carousel').owlCarousel({
+        nav: true,
+        dots: false,
+        margin: 40,
+        loop: true,
+        navText: ["<span><i class='fas fa-arrow-left'></i> Previous</span>", "<span>Next <i class='fas fa-arrow-right'></i></span>"],
         responsive:{
             0:{
                 items:1
             },
-
             600:{
                 items:2
             },
             1000:{
                 items:3
-            },
-
-
+            }
         }
     });
 
-     //favorite owl carousel
-     $('#favorite .owl-carousel').owlCarousel({
-        margin:40,
-        dots:false,
-        nav:true,
-        loop:true,
-        navText:["<span><i class='fas fa-arrow-left'></i>Previous</span>","<span>Next<i class='fas fa-arrow-right'></i></span>"],
-        responsive:{
-            0:{
-                items:1
-            },
 
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
-            },
+        // security owl carousel
+        $('#favourite .owl-carousel').owlCarousel({
+            nav: true,
+            dots: false,
+            margin: 20,
+            loop: true,
+            navText: ["<span><i class='fas fa-arrow-left'></i></span>", "<span><i class='fas fa-arrow-right'></i></span>"],
+            responsive:{
+                0:{
+                    items:1
+                },
+                500:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        });
 
 
-        }
-    });
 });
